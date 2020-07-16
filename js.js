05cb0cc8ce897605d9c397f9bf6679a7c1d93a32
Leviosah <conormag60@gmail.com>
2020-07-15 21:03:06 -0700

    first commit

---
+++ js.js
@@ -0,0 +1,16 @@
+// Planner JS
+
+
+const cardContainer = $(".container")
+// current date + time
+
+let m = moment();
+
+console.
+
+// function for creating dynamic cards
+
+function cardGenerator() {
+    
+}
+

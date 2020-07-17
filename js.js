c06995e5f0c5d3d0f5e0353f8427261a38153c14
Leviosah <conormag60@gmail.com>
2020-07-16 23:30:12 -0700

    final v1 commit

--- js.js
+++ js.js
@@ -1,16 +1,53 @@
-// Planner JS
 
+var today = moment().format('MMMM Do YYYY');
+console.log(today);
 
-const cardContainer = $(".container")
-// current date + time
+let dateHeader = $('#currentDay');
+dateHeader.text(today);
 
-let m = moment();
+let inputText = document.getElementById("textInput");
+console.log(inputText)
 
-console.
+// getting the current hour to set coloring
+var currentHour = new Date().getHours();
 
-// function for creating dynamic cards
+console.log(currentHour)
 
-function cardGenerator() {
-    
-}
+// for loop building the elements, setting the time/color conditions, and save event listener
+for(i=9; i<17; i++) {
+  console.log(moment(i, "HH").format('hh a'));
+  var description = localStorage.getItem(i);
+  if(description==null)
+  {
+    description="";
+  }
+  if(i<currentHour)
+  {
+    blockColor="past";
+  }
+  else if(i==currentHour)
+  {
+    blockColor="present"
+  }
+  else{
+    blockColor="future"
+  }
+  $("#schedule").append("<tr><td class=hour>"+moment(i, "HH").format('hh a')+"<td class="+blockColor+"><textarea data-hour"+i+" id = workHour" + i + ">"+description+"</textarea></td><td class=saveBtn id=saveHour" + i + "><i id=saveHour"+i+">Save</i></td></tr>");
+};
 
+// save function saving input in user val and then setting item to local storage
+$(".saveBtn").on("click", function(event) {
+    var idtext = event.target.id;
+    var i = idtext.substring(8,10);
+    event.preventDefault();
+    var userInput = $("#workHour"+i).val();
+    console.log("below is user input");
+    console.log(userInput);
+    console.log("the below is i");
+    console.log(i);
+    localStorage.setItem(i, userInput);
+
+});
+
+console.log("this is i " + i);
+console.log(localStorage)
